10f5958f257b3e8bec227a67b1572860a166690d
Lyee-Git <70620139+Lyee-Git@users.noreply.github.com>
2023-02-26 00:36:13 +0800

    dp4 stock problems

---
+++ 309.最佳买卖股票时机含冷冻期.java
@@ -0,0 +1,30 @@
+
+/*
+ * @lc app=leetcode.cn id=309 lang=java
+ *
+ * [309] 最佳买卖股票时机含冷冻期
+ */
+import java.util.*;
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            } else if (i - 2 == -1) {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]); // 第一天和第二天选一天买入
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+}
+// @lc code=end
