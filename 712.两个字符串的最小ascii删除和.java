e073f3a963bf46bbb3b6684d60aeccc004a86103
Lyee-Git <70620139+Lyee-Git@users.noreply.github.com>
2023-05-02 23:57:58 +0800

    2023.5.2

--- 712.两个字符串的最小ascii删除和.java
+++ 712.两个字符串的最小ascii删除和.java
@@ -12,11 +12,31 @@ class Solution {
 
     public int minimumDeleteSum(String s1, String s2) {
         int m = s1.length(), n = s2.length();
-        dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+        // dp = new int[m][n];
+        // for (int i = 0; i < m; i++) {
+        // Arrays.fill(dp[i], -1);
+        // }
+        // return minDelSum(s1, 0, s2, 0);
+
+        // dp[i][j] for min DeleteSum between s1[0..i - 1] and s2[0..j - 1]
+        dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] + s1.charAt(i - 1);
         }
-        return minDelSum(s1, 0, s2, 0);
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] + s2.charAt(j - 1);
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j] + s1.charAt(i - 1),
+                            dp[i][j - 1] + s2.charAt(j - 1));
+                }
+            }
+        }
+        return dp[m][n];
     }
 
     // 返回s1[i..]和s2[j..]的最小删除和
